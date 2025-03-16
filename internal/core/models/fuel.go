5726af057b68a8ecef4a40ffd7bea1c6a9b7e340
johnfercher <johnathanfercher22@gmail.com>
2025-03-16 11:14:12 -0300

    Add fuel

--- internal/core/models/fuel.go
+++ internal/core/models/fuel.go
@@ -2,105 +2,100 @@ package models
 
 type Fuel struct {
 	Name       string
-	MinOctane  float64
-	MaxOctane  float64
+	Octane     float64
 	LatentHeat float64
 	HeatPower  float64
 	AFRatio    float64
 }
 
-func (f *Fuel) PowerDiffTo(ref *Fuel) float64 {
-	return 0.0
+func (f *Fuel) GetPotential() float64 {
+	afHeatPower := f.HeatPower / f.AFRatio
+	return afHeatPower * f.Octane
 }
 
-func (f *Fuel) AVGOctane() float64 {
-	return (f.MinOctane + f.MaxOctane) / 2.0
+func (f *Fuel) Compare(ref *Fuel) float64 {
+	fix := 0.85
+	value := f.GetPotential() / ref.GetPotential()
+	if value > 1 {
+		sup := value - 1
+		sup *= fix
+		calc := 1 + sup
+		return calc
+	}
+
+	sub := 1 - value
+	sub *= fix
+	calc := 1 - sub
+	return calc
 }
 
-func BRGasoline() *Fuel {
+func Mix(name string, a *Fuel, percentA float64, b *Fuel, percentB float64) *Fuel {
 	return &Fuel{
-		Name:       "BR Gasoline",
-		MinOctane:  92,
-		MaxOctane:  92,
-		LatentHeat: 350,
-		AFRatio:    13.56,
-		HeatPower:  11.220,
+		Name:       name,
+		Octane:     (a.Octane * percentA / 100.0) + (b.Octane * percentB / 100.0),
+		LatentHeat: (a.LatentHeat * percentA / 100.0) + (b.LatentHeat * percentB / 100.0),
+		HeatPower:  (a.HeatPower * percentA / 100.0) + (b.HeatPower * percentB / 100.0),
+		AFRatio:    (a.AFRatio * percentA / 100.0) + (b.AFRatio * percentB / 100.0),
 	}
 }
 
 func Gasoline100() *Fuel {
 	return &Fuel{
 		Name:       "Gasoline 100%",
-		MinOctane:  87,
-		MaxOctane:  87,
+		Octane:     87,
 		LatentHeat: 350,
 		AFRatio:    14.57,
-		HeatPower:  11.220,
+		HeatPower:  11200,
 	}
 }
 
-func VpGasolineC16() *Fuel {
+func E85() *Fuel {
+	gasoline100 := Gasoline100()
+	e100 := E100()
+
+	return Mix("E85", e100, 85, gasoline100, 15)
+}
+
+func E100() *Fuel {
 	return &Fuel{
-		Name:       "VP Gasoline C16",
-		MinOctane:  120,
-		MaxOctane:  120,
-		LatentHeat: 350,
-		AFRatio:    14.57,
-		HeatPower:  11.220,
+		Name:       "Ethanol 100%",
+		Octane:     110,
+		LatentHeat: 900,
+		AFRatio:    8.99,
+		HeatPower:  6507,
 	}
+}
+
+func BRGasoline() *Fuel {
+	gasoline100 := Gasoline100()
+	e100 := E100()
+
+	return Mix("BR Gasoline", e100, 22, gasoline100, 78)
 }
 
 func BRPodiumGasoline() *Fuel {
-	return &Fuel{
-		Name:       "BR Podium Gasoline",
-		MinOctane:  102,
-		MaxOctane:  102,
-		LatentHeat: 350,
-		AFRatio:    13.56,
-		HeatPower:  11.220,
-	}
+	gasoline100 := Gasoline100()
+	e100 := E100()
+
+	return Mix("BR Podium Gasoline", e100, 67, gasoline100, 33)
 }
 
-func BRAlcohol() *Fuel {
+func BREthanol() *Fuel {
 	return &Fuel{
 		Name:       "BR Alcohol",
-		MinOctane:  110,
-		MaxOctane:  110,
+		Octane:     110,
 		LatentHeat: 900,
 		AFRatio:    8.5, // Hidratado
-		HeatPower:  6.507,
+		HeatPower:  6507,
 	}
 }
 
 func BRMethanol() *Fuel {
 	return &Fuel{
 		Name:       "BR Methanol",
-		MinOctane:  115,
-		MaxOctane:  115,
+		Octane:     115,
 		LatentHeat: 1100,
 		AFRatio:    6.47,
-		HeatPower:  5.330,
-	}
-}
-
-func E85() *Fuel {
-	return &Fuel{
-		Name:       "E85",
-		MinOctane:  100,
-		MaxOctane:  105,
-		LatentHeat: 900,
-		AFRatio:    8.99,
-		HeatPower:  6.507,
-	}
-}
-
-func E100() *Fuel {
-	return &Fuel{
-		Name:       "Alcohol 100%",
-		MinOctane:  110,
-		MaxOctane:  110,
-		LatentHeat: 900,
-		AFRatio:    8.99,
-		HeatPower:  6.507,
+		HeatPower:  5330,
 	}
 }
